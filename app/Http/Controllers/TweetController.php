6fc52e49ca49938b8a4b7560865e6ac18486fe25
Takuto <htakutolife@gmail.com>
2023-06-02 17:31:51 +0900

    tweetコントローラー作成

---
+++ app/Http/Controllers/TweetController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Tweet;
+
+class TweetController extends Controller
+{
+    public function index(){
+        $tweets = Tweet::all();
+        return view('tweets.index', compact('tweets'));
+    }
+}
