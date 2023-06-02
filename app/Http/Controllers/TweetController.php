b52ee3f16f2546bde9f4caea9cae049359540c55
Takuto <htakutolife@gmail.com>
2023-06-02 19:30:02 +0900

    詳細コントローラー作成

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -43,4 +43,10 @@ class TweetController extends Controller
         $tweet->update($request->all());
         return view('tweets.update');
     }
+
+    public function show($id)
+    {
+        $tweet = Tweet::find($id);
+        return view('tweets.show', compact('tweet'));
+    }
 }
